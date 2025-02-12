09847257a9f0c098105830c44fef7c1a29d916cc
Michael Sylvest Bendtsen <54103011+Michael30502@users.noreply.github.com>
2025-02-12 19:31:08 +0100

    Player can move

--- Assets/Scripts/Spaces/Player.cs
+++ Assets/Scripts/Spaces/Player.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
 using System;
 using System.Collections;
+using UnityEngine.UIElements;
 public class Player : MonoBehaviour
 {
 
     public SpaceClass currentSpace;
-
-    Player(SpaceClass currentSpace) {
-        this.currentSpace = currentSpace;
-    }
+    private int spaceToMove=0;
     
     public SpaceClass makeChoice(ArrayList nextSpaces) {
 
@@ -16,6 +14,35 @@ public class Player : MonoBehaviour
         return (SpaceClass)nextSpaces[0];
 
     }
-    
-    
+
+    public void moveNSpaces(int n) {
+
+        spaceToMove = n;
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        print(currentSpace);
+        Vector3 tempPos = currentSpace.transform.position;
+        tempPos.y += 0.5f;
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, tempPos, 1 * Time.deltaTime);
+        StartCoroutine(SwapSpace());
+        
+        
+    }
+
+
+    IEnumerator SwapSpace() {
+        yield return new WaitForSeconds(5.0f);
+        currentSpace = currentSpace.nextSpaces;
+
+
+    }
+
 }
