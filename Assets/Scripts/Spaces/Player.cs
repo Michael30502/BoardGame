203999624c3eec8504d41343d6270d6ffc3414e8
mfs20 <mfs2001@hotmail.dk>
2025-02-20 17:18:50 +0100

    update scripts, create dice face, short anim and fix Micheals camera angle.

--- Assets/Scripts/Spaces/Player.cs
+++ Assets/Scripts/Spaces/Player.cs
@@ -2,62 +2,55 @@ using UnityEngine;
 using System;
 using System.Collections;
 using UnityEngine.UIElements;
+
 public class Player : MonoBehaviour
 {
-
     public SpaceClass currentSpace;
-    private int spaceToMove=0;
+    private int spaceToMove = 0;
     private bool block = false;
-    
-    public SpaceClass makeChoice(ArrayList nextSpaces) {
+    public Dice dice;  // Reference to the Dice script
 
-
-        return (SpaceClass)nextSpaces[0];
-
-    }
-
-    public void moveNSpaces(int n) {
-
-        StartCoroutine(SwapSpace(n));
-
-    }
-
-    private void Start()
+    public SpaceClass makeChoice(ArrayList nextSpaces)
     {
+        return (SpaceClass)nextSpaces[0];
+    }
+
+    public void moveNSpaces(int n)
+    {
+        StartCoroutine(SwapSpace(n)); 
     }
 
     private void Update()
     {
-        //print(currentSpace);
+        // Move player smoothly to current space
         Vector3 tempPos = currentSpace.transform.position;
         tempPos.y += 0.5f;
         gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, tempPos, 1 * Time.deltaTime);
 
-        if (Input.GetKeyUp(KeyCode.Space) && block ==false)
+        if (Input.GetKeyUp(KeyCode.Space) && block == false)
         {
             int ran = UnityEngine.Random.Range(1, 7);
-            print("Rolled" +ran);
-            moveNSpaces(ran);
+            print("Rolled " + ran);
+
+            dice.StartRolling();
+            StartCoroutine(SwapSpace(ran));
             block = true;
         }
-
     }
 
-
-    IEnumerator SwapSpace(int n) {
+    IEnumerator SwapSpace(int n)
+    {
+       //roll first, then walk anima
         yield return new WaitForSeconds(1.0f);
-        for (; n > 0; n--) { 
-        
-        currentSpace = currentSpace.nextSpaces;
-            yield return new WaitForSeconds(2.0f);
+        dice.StopRolling(n); 
 
+        yield return new WaitForSeconds(0.5f);
+        for (; n > 0; n--)
+        {
+            currentSpace = currentSpace.nextSpaces;
+            yield return new WaitForSeconds(1.5f);
         }
         block = false;
-        print("ready");
-
+        print("Ready");
     }
-
-
 }
-
-
