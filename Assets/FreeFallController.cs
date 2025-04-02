9d833134f13c2fe6526a9b28ed0e81a6a6088294
mathias124 <mfs2001@hotmail.dk>
2025-04-02 14:21:07 +0200

    push split screen again

---
+++ Assets/FreeFallController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FreeFallController : MonoBehaviour
+{
+    [SerializeField] private FreeFallCharacter[] playerList;
+
+    void Start()
+    {
+        AssignGamepads();
+    }
+
+    private void AssignGamepads()
+    {
+        int gamepadIndex = 0; // Keep track of assigned gamepads
+
+        foreach (var player in playerList)
+        {
+            if (gamepadIndex < Gamepad.all.Count)
+            {
+                player.SetGamepad(Gamepad.all[gamepadIndex]); // Assign gamepad in order
+                gamepadIndex++; // Move to the next available gamepad
+            }
+            else
+            {
+                player.SetGamepad(null); // No gamepad available
+            }
+        }
+    }
+}
--- Assets/Scripts/MiniGame 1/FreeFallCharacter.cs
+++ Assets/Scripts/MiniGame 1/FreeFallCharacter.cs
@@ -1,31 +1,54 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FreeFallCharacter : MonoBehaviour
 {
     public float fallSpeed = 100f;
-    public float moveSpeed = 45f; 
-    public float gravity = 35f; 
+    public float moveSpeed = 10f;
+    public float gravity = 9.81f;
 
-    private CharacterController controller;
+    private Rigidbody rb;
+    private CapsuleCollider col;
     private Vector3 moveDirection;
+    private Gamepad assignedGamepad = null;
+
+    public void SetGamepad(Gamepad gamepad)
+    {
+        assignedGamepad = gamepad;
+    }
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+
+        rb.useGravity = false;  // We manually handle gravity
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        //Input fra player
-        float moveX = Input.GetAxis("Horizontal"); 
-        float moveZ = Input.GetAxis("Vertical"); 
+        float moveX = 0f;
+        float moveZ = 0f;
 
-       
-        moveDirection.x = moveX * moveSpeed;
-        moveDirection.z = moveZ * moveSpeed;
-        moveDirection.y = -fallSpeed - gravity * Time.deltaTime; 
+        if (assignedGamepad != null) // ONLY use the assigned gamepad
+        {
+            moveX = assignedGamepad.leftStick.x.ReadValue();
+            moveZ = assignedGamepad.leftStick.y.ReadValue();
+        }
+        else
+        {
+            moveX = Input.GetAxis("Horizontal");
+            moveZ = Input.GetAxis("Vertical");
+        }
 
-      
-        controller.Move(moveDirection * Time.deltaTime);
+        Vector3 inputVector = new Vector3(moveX, 0, moveZ);
+        if (inputVector.magnitude > 1f)
+            inputVector.Normalize();
+
+        moveDirection = inputVector * moveSpeed;
+        
+
+        rb.linearVelocity = new Vector3(moveDirection.x, rb.linearVelocity.y - (gravity * Time.fixedDeltaTime), moveDirection.z);
     }
 }
