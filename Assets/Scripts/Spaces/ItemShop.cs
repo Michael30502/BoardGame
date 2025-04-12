64ddb260d1bfff4ba706fd6c9b200af4bf2be537
Mathias Schrader /Metamorphosis <54176496+mathias124@users.noreply.github.com>
2025-04-12 02:33:51 +0200

    Push Physical Shop, Demo. Removes Physical Item upon purchase, needs hovering and price tag.

--- Assets/Scripts/Spaces/ItemShop.cs
+++ Assets/Scripts/Spaces/ItemShop.cs
@@ -1,73 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ItemShop : MonoBehaviour, SpaceActions
 {
-
+  
     public List<Item> shop = new List<Item>();
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] public GameObject ShopWindowCamera;
+
+    [SerializeField] private List<GameObject> shopItem3DObjects;
+
+    
+    private AudioSource audioSource;
+
+    [SerializeField] private AudioClip GreetingsCustomerClip;
+    [SerializeField] private AudioClip ThankYouComeAgainClip;
+    [SerializeField] private AudioClip BuyBeerClip;
+    [SerializeField] private AudioClip BuyD10Clip;
+
+   
     public void Action(Player player)
     {
+        audioSource = GetComponent<AudioSource>();
+
+        if (ShopWindowCamera != null)
+            ShopWindowCamera.SetActive(true);
 
         StartCoroutine(Shop(player));
-
-
-
     }
 
+    
     public bool GetCountSpace()
     {
         return false;
     }
 
+   
+    IEnumerator Shop(Player player)
+    {
+        Debug.Log("Buy Something!!!");
 
-    IEnumerator Shop(Player player) {
-        print("Buy Something!!!");
+        if (GreetingsCustomerClip != null)
+            audioSource.PlayOneShot(GreetingsCustomerClip);
+
         int itemSelected = 0;
 
-
-        while (true) {
+        while (true)
+        {
             int tempValue = ChooseOption.Choose(itemSelected, shop.Count, player.gamepad);
             if (itemSelected != tempValue)
-        {
-            itemSelected = tempValue;
-            print(shop[itemSelected].name + " " + shop[itemSelected].price+"$");
-        }
+            {
+                itemSelected = tempValue;
+                Debug.Log(shop[itemSelected].name + " " + shop[itemSelected].price + "$");
+            }
 
             if (ChooseOption.Select(player.gamepad))
             {
                 if (player.money >= shop[itemSelected].price)
                 {
                     player.items.Add((Item)shop[itemSelected].Clone());
+                    player.money -= shop[itemSelected].price;
                     player.playerAction = false;
-                    player.money-= shop[itemSelected].price;
-                    print("You bought: " + shop[itemSelected].name+" Thank you come again");
+
+                    Debug.Log("You bought: " + shop[itemSelected].name + " Thank you come again");
+
+                   
+                    if (ThankYouComeAgainClip != null)
+                        audioSource.PlayOneShot(ThankYouComeAgainClip);
+
+                    
+                    if (shopItem3DObjects != null && itemSelected < shopItem3DObjects.Count)
+                    {
+                        shopItem3DObjects[itemSelected].SetActive(false);
+                    }
+
+                   
+                    if (shop[itemSelected].name == "Duff Beer" && BuyBeerClip != null)
+                        audioSource.PlayOneShot(BuyBeerClip);
+                    else if (shop[itemSelected].name == "D10" && BuyD10Clip != null)
+                        audioSource.PlayOneShot(BuyD10Clip);
+
                     break;
                 }
-                else {
-                    print("No money");
+                else
+                {
+                    Debug.Log("No money");
                 }
-
-              
-
             }
 
             if (ChooseOption.Cancel(player.gamepad))
             {
                 player.playerAction = false;
                 break;
-
             }
 
-
             yield return null;
+        }
 
+        if (ShopWindowCamera != null)
+            ShopWindowCamera.SetActive(false);
     }
-
-
-}
-
 }
