ac7e41b4fc089cafd68423f589a5e1ff4171e487
Michael Sylvest Bendtsen <mich.bendtsen@gmail.com>
2025-05-17 02:19:19 +0200

    connection between minigame 1 and boardgame

--- Assets/Scripts/MiniGame 1/FallMeter.cs
+++ Assets/Scripts/MiniGame 1/FallMeter.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using System.Collections;
 
 public class FallMeter : MonoBehaviour
 {
@@ -17,13 +20,34 @@ public class FallMeter : MonoBehaviour
 
     private HashSet<int> winningPlayers = new HashSet<int>();
 
-
+    [SerializeField] private string scene;
     private bool colorRandomizerEnabled = false;
 
 
     private const float startY = 2352f;
     private List<float> lastKnownDistances;
     private bool scoreboardShown = false;
+    MinigameManager minigameManager;
+
+
+    private void Start()
+    {
+        try
+        {
+            minigameManager = GameObject.Find("MinigameController").GetComponent<MinigameManager>();
+            if (minigameManager != null)
+            {
+                minigameManager.mainSceneParent.SetActive(false);
+            }
+        }
+        catch (System.Exception e) { print("main scene not found"); }
+
+    }
+
+
+
+
+
 
     private void Awake()
     {
@@ -91,8 +115,29 @@ public class FallMeter : MonoBehaviour
         if (scoreBoardObject != null)
             scoreBoardObject.SetActive(true);
 
+        gameObject.SetActive(true);
+        StartCoroutine(WaitForInput());
         scoreboardShown = true;
+
+
+       
     }
+
+    IEnumerator WaitForInput()
+    {
+        Input.ResetInputAxes();
+
+        while (true)
+        {
+
+            if (InputManager.InputSelect(Gamepad.current))
+                EndGame();
+            Input.ResetInputAxes();
+
+            yield return null;
+        }
+    }
+
 
     public void RegisterWin(int playerIndex)
     {
@@ -102,8 +147,15 @@ public class FallMeter : MonoBehaviour
             winningPlayers.Add(playerIndex);
         }
     }
-
-
+    public void EndGame()
+    {
+        if (minigameManager != null)
+        {
+            minigameManager.mainSceneParent.SetActive(true);
+            minigameManager.SetPlayerMoney(new List<int> { GetSortedFallResults()[0].playerIndex, GetSortedFallResults()[1].playerIndex, GetSortedFallResults()[2].playerIndex, GetSortedFallResults()[3].playerIndex });
+            SceneManager.UnloadSceneAsync(scene);
+        }
+    }
     public List<(int playerIndex, float distance)> GetSortedFallResults()
 {
     return freeFallCharacters
